b531b1a19e5e5ce5a6cdb38795017c171f34991c
Mohamed Moussa <mohamed.moussa2@dxc.com>
2025-03-19 21:50:32 +0200

    add ProdcutDetailsComponent

--- Angular-ecommerce/src/app/routes.ts
+++ Angular-ecommerce/src/app/routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 export const routes: Routes = [
   {
     path: 'category/:id',
     component: ProductListComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent,
   },
   {
     path: 'search/:keyword',
--- Angular-ecommerce/src/app/services/product.service.ts
+++ Angular-ecommerce/src/app/services/product.service.ts
@@ -11,17 +11,21 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) {}
 
+  getProduct(productId: Number): Observable<Product> {
+    const productUrl = `${this.baseUrl}/${productId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
   getProductList(categoryId: Number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
-    return this.getProucts(searchUrl);
+    return this.getProudcts(searchUrl);
   }
 
   searchProducts(keyword: string): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`;
-    return this.getProucts(searchUrl);
+    return this.getProudcts(searchUrl);
   }
 
-  getProucts(searchUrl: string) {
+  getProudcts(searchUrl: string) {
     return this.httpClient
       .get<GetResponseProduct>(searchUrl)
       .pipe(map((response) => response._embedded.products));
