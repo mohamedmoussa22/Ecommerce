b95dff0a41fdefcfbd215bfef13a76da91f47934
Mohamed Moussa <mohamed.moussa2@dxc.com>
2025-04-19 15:12:02 +0200

    Integrate countries and states with UI

--- Angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Luv2ShopFormService } from '../../services/luv2-shop-form.service';
+import { Country } from '../../common/country';
+import { CommonModule } from '@angular/common';
+import { State } from '../../common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -11,6 +15,15 @@ export class CheckoutComponent implements OnInit {
   checkoutFormGroup!: FormGroup;
   totalPrice: number = 0;
   totalQuantity: number = 0;
+
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
+  constructor(private luv2ShopService: Luv2ShopFormService) {}
 
   ngOnInit(): void {
     this.checkoutFormGroup = new FormGroup({
@@ -42,6 +55,27 @@ export class CheckoutComponent implements OnInit {
         expirationYear: new FormControl(''),
       }),
     });
+
+    // populate credit card months
+
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log('startMonth: ' + startMonth);
+
+    this.luv2ShopService.getCreditCardMonths(startMonth).subscribe((date) => {
+      console.log('Months: ' + JSON.stringify(date));
+      this.creditCardMonths = date;
+    });
+
+    // populate credit card years
+    this.luv2ShopService.getCreditCardYears().subscribe((data) => {
+      console.log('Years: ' + JSON.stringify(data));
+      this.creditCardYears = data;
+    });
+
+    this.luv2ShopService.getCountries().subscribe((data) => {
+      console.log('Reterived Countries: ' + JSON.stringify(data));
+      this.countries = data;
+    });
   }
 
   onSubmit() {
@@ -54,8 +88,45 @@ export class CheckoutComponent implements OnInit {
       this.checkoutFormGroup.controls['billingAddress'].setValue(
         this.checkoutFormGroup.controls['shippingAddress'].value
       );
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls['billingAddress'].reset();
+      this.billingAddressStates = [];
     }
+  }
+
+  handleMonthsBasedOnYear($event: Event) {
+    const selectedYear = Number((<HTMLInputElement>$event.target).value);
+    const currentYear: number = new Date().getFullYear();
+    let startDate: number;
+    if (selectedYear > currentYear) {
+      startDate = 1;
+    } else {
+      startDate = new Date().getMonth() + 1;
+    }
+    this.luv2ShopService.getCreditCardMonths(startDate).subscribe((data) => {
+      console.log('Months: ' + JSON.stringify(data));
+      this.creditCardMonths = data;
+    });
+  }
+
+  getStates(formGroupName: string) {
+    console.log('retrieving states from country code: ' + formGroupName);
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+    const countryName = formGroup?.value.country.name;
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.luv2ShopService.getStates(countryCode).subscribe((data) => {
+      if (formGroupName === 'shippingAddress') {
+        this.shippingAddressStates = data;
+      } else {
+        this.billingAddressStates = data;
+      }
+      formGroup?.get('state')?.setValue(data[0]);
+    });
+
+
   }
 }
